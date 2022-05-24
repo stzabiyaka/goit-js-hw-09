5ec1f4faed0acc924c1720556586ffea7a6f5b8b
Stanislav Zabiyaka <stzabiyaka@gmail.com>
2022-05-24 19:35:16 +0300

    01-color-switcher.js wrote

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
 
+import getRandomHexColor from "./random-hex-color";
+
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.body
+}
+
+let timerId = null;
+
+onStopBtnClick();
+
+function changeBodyColor () {
+    refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStartBtnClick () {
+    timerId = setInterval(changeBodyColor, 1000);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    refs.stopBtn.addEventListener('click', onStopBtnClick);
+    refs.stopBtn.removeEventListener('click', onStartBtnClick);
+}
+    
+function onStopBtnClick () {
+    clearInterval(timerId);
+    refs.stopBtn.disabled = true;
+    refs. startBtn.disabled = false;
+    refs.startBtn.addEventListener('click', onStartBtnClick);
+    refs.startBtn.removeEventListener('click', onStopBtnClick);
+}
